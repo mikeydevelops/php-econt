5760d1a0ff6b7335f06ef62a1ac616319c5e9b45
Mihail Ivanov <bulgedition@gmail.com>
2025-01-05 15:07:32 +0200

    refactored enum constant names to camelCase from snake_case

--- src/Enums/ServiceType.php
+++ src/Enums/ServiceType.php
@@ -7,48 +7,48 @@ use MikeyDevelops\Econt\Enums\Enum;
 /**
  * A type of service.
  *
- * @method  static  from_door_courier()
- * @method  static  to_door_courier()
- * @method  static  from_office_courier()
- * @method  static  to_office_courier()
- * @method  static  from_door_cargo()
- * @method  static  to_door_cargo()
- * @method  static  from_office_cargo()
- * @method  static  to_office_cargo()
- * @method  static  from_door_post()
- * @method  static  to_door_post()
- * @method  static  from_office_post()
- * @method  static  to_office_post()
+ * @method  static  fromDoorCourier()
+ * @method  static  toDoorCourier()
+ * @method  static  fromOfficeCourier()
+ * @method  static  toOfficeCourier()
+ * @method  static  fromDoorCargo()
+ * @method  static  toDoorCargo()
+ * @method  static  fromOfficeCargo()
+ * @method  static  toOfficeCargo()
+ * @method  static  fromDoorPost()
+ * @method  static  toDoorPost()
+ * @method  static  fromOfficePost()
+ * @method  static  toOfficePost()
  * @method  static  code1()
- * @method  static  from_door_cargo_expres()
- * @method  static  to_door_cargo_expres()
- * @method  static  from_office_cargo_expres()
- * @method  static  to_office_cargo_expres()
- * @method  static  to_door_trans_pallet()
- * @method  static  to_office_trans_pallet()
+ * @method  static  fromDoorCargoExpres()
+ * @method  static  toDoorCargoExpres()
+ * @method  static  fromOfficeCargoExpres()
+ * @method  static  toOfficeCargoExpres()
+ * @method  static  toDoorTransPallet()
+ * @method  static  toOfficeTransPallet()
  *
  * Enum was based on servingType in:
  * @see https://ee.econt.com/services/Nomenclatures/#ServingOfficeElement
  */
 class ServiceType extends Enum
 {
-    private const from_door_courier = 'from_door_courier';
-    private const to_door_courier = 'to_door_courier';
-    private const from_office_courier = 'from_office_courier';
-    private const to_office_courier = 'to_office_courier';
-    private const from_door_cargo = 'from_door_cargo';
-    private const to_door_cargo = 'to_door_cargo';
-    private const from_office_cargo = 'from_office_cargo';
-    private const to_office_cargo = 'to_office_cargo';
-    private const from_door_post = 'from_door_post';
-    private const to_door_post = 'to_door_post';
-    private const from_office_post = 'from_office_post';
-    private const to_office_post = 'to_office_post';
+    private const fromDoorCourier = 'from_door_courier';
+    private const toDoorCourier = 'to_door_courier';
+    private const fromOfficeCourier = 'from_office_courier';
+    private const toOfficeCourier = 'to_office_courier';
+    private const fromDoorCargo = 'from_door_cargo';
+    private const toDoorCargo = 'to_door_cargo';
+    private const fromOfficeCargo = 'from_office_cargo';
+    private const toOfficeCargo = 'to_office_cargo';
+    private const fromDoorPost = 'from_door_post';
+    private const toDoorPost = 'to_door_post';
+    private const fromOfficePost = 'from_office_post';
+    private const toOfficePost = 'to_office_post';
     private const code1 = 'code1';
-    private const from_door_cargo_expres = 'from_door_cargo_expres';
-    private const to_door_cargo_expres = 'to_door_cargo_expres';
-    private const from_office_cargo_expres = 'from_office_cargo_expres';
-    private const to_office_cargo_expres = 'to_office_cargo_expres';
-    private const to_door_trans_pallet = 'to_door_trans_pallet';
-    private const to_office_trans_pallet = 'to_office_trans_pallet';
+    private const fromDoorCargoExpres = 'from_door_cargo_expres';
+    private const toDoorCargoExpres = 'to_door_cargo_expres';
+    private const fromOfficeCargoExpres = 'from_office_cargo_expres';
+    private const toOfficeCargoExpres = 'to_office_cargo_expres';
+    private const toDoorTransPallet = 'to_door_trans_pallet';
+    private const toOfficeTransPallet = 'to_office_trans_pallet';
 }
--- src/Enums/ShipmentType.php
+++ src/Enums/ShipmentType.php
@@ -9,14 +9,14 @@ use MikeyDevelops\Econt\Enums\Enum;
  *
  * @method  static  document()  Documents (up to 0.5kg)
  * @method  static  pack()  Parcel (up to 50kg)
- * @method  static  post_pack()  Post parcel (up to 20kg, 60x60x60cm and subcode = office-office)
+ * @method  static  postPack()  Post parcel (up to 20kg, 60x60x60cm and subcode = office-office)
  * @method  static  pallet()  Pallet (80x120x180cm and up to 1000kg)
  * @method  static  cargo()  Cargo express (palletized shipment over 80x120x180cm up to 200x200x180cm and up to 500kg)
- * @method  static  documentpallet()  Pallet + documents
- * @method  static  big_letter()  Big Letter
- * @method  static  small_letter()  Small Letter
- * @method  static  money_transfer()  Money transfer
- * @method  static  pp()  Post transfer
+ * @method  static  documentPallet()  Pallet + documents
+ * @method  static  bigLetter()  Big Letter
+ * @method  static  smallLetter()  Small Letter
+ * @method  static  moneyTransfer()  Money transfer
+ * @method  static  postTransfer()  Post transfer
  *
  * @see https://ee.econt.com/services/Shipments/#ShipmentType
  */
@@ -24,12 +24,12 @@ class ShipmentType extends Enum
 {
     private const document = 'document';
     private const pack = 'pack';
-    private const post_pack = 'post_pack';
+    private const postPack = 'post_pack';
     private const pallet = 'pallet';
     private const cargo = 'cargo';
-    private const documentpallet = 'documentpallet';
-    private const big_letter = 'big_letter';
-    private const small_letter = 'small_letter';
-    private const money_transfer = 'money_transfer';
-    private const pp = 'pp';
+    private const documentPallet = 'documentpallet';
+    private const bigLetter = 'big_letter';
+    private const smallLetter = 'small_letter';
+    private const moneyTransfer = 'money_transfer';
+    private const postTransfer = 'pp';
 }
